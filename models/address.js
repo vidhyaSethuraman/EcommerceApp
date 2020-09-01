e2d287f575bc2c0a1baab0576bfbcd6dcf196601
vidhya1204 <47454217+vidhyaSethuraman@users.noreply.github.com>
2020-09-01 14:59:13 +0530

    Add files via upload

---
+++ models/address.js
@@ -0,0 +1,46 @@
+var mongoose = require('mongoose');
+
+const addressSchema = new mongoose.Schema(
+    {
+
+        customer_id:
+        {
+            type:String,
+            required:true
+        },
+        pincode: {
+            type: Number,
+            minlength: 6,
+            required: true
+        },
+        details: {
+            type:String,
+            required:true
+        },
+        landmark: {
+            type:String
+        },
+        city: {
+            type: String,
+            required:true
+        },
+        state: {
+            type:String,
+            required:true
+        },
+        defaulta: {
+            type: Boolean
+        },
+        home_address: {
+            type:Boolean
+        },
+        office_address: {
+            type:Boolean
+        }
+        
+    }
+)
+
+const Address = mongoose.model('address', addressSchema);
+
+module.exports = Address;
